71c6ed6895699cb6864db9e7d4ec20a527a48555
Sam Garcia <samuel.gsuero@gmail.com>
2023-01-30 11:06:28 +0100

    API call updated to match v2

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', 'f2e4a898bbmsh5b73b79b065ca26p1d9141jsn02663139a68c');
 
@@ -13,9 +13,10 @@ export const shazamCoreApi = createApi({
     }
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
-    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}`}),
-    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}`}),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}`}),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
   })
 });
 
@@ -23,4 +24,5 @@ export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
 } = shazamCoreApi;

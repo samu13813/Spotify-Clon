21dfac2a62234964da30bb8af3c25c5d51ae76a0
Sam Garcia <samuel.gsuero@gmail.com>
2023-01-30 16:19:02 +0100

    searchbar functionality added
    search bar now allows typing on it and searching

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+const Searchbar = () => {
+
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevents reload of page when submitting form
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+  <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
     <label htmlFor="search-field" className="sr-only">
       Search all songs
     </label>
@@ -15,12 +26,13 @@ const Searchbar = () => (
         id="search-field"
         placeholder="Search"
         type="search"
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
       />
     </div>
   </form>
-);
+  )
+};
 
 export default Searchbar;
